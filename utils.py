e8476f27ccfa6e2de39b422f318e5af29b95f5d0
Sara Paguaga <64711979+MGonza20@users.noreply.github.com>
2024-01-23 07:19:24 -0600

    Fix

--- utils.py
+++ utils.py
@@ -13,7 +13,8 @@ def plot_eq(img, eq=False):
     fda = hist.cumsum() # Funcion de distribucion acumulada (FDA)
 
     if eq:
-        fda_norm = fda * hist.max() / fda.max()
+        fda_norm = (fda - fda.min()) * 255 / (fda.max() - fda.min())
+
         img = fda_norm[img.flatten()].reshape(img.shape) # Mapeando los valores de la imagen
         hist, _ = np.histogram(img.flatten(), 256, [0,256])
         fda = hist.cumsum()
